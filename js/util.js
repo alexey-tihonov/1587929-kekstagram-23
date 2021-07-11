ca627312a156abae435064a0b0b9bcc83287825b
alexey-tihonov <alexey.tihonov@travelline.ru>
2021-07-11 19:27:06 +0300

    Реализация функицонала отображение дополнительных комментариев

--- js/util.js
+++ js/util.js
@@ -1,11 +1,10 @@
 const ESC_KEYCODE = 27;
+const ENTER_KEYCODE = 13;
 const keyboard = {
   isEsc: (e, callBack) => e.keyCode === ESC_KEYCODE && callBack(),
+  isEnter: (e, callBack) => e.keyCode === ENTER_KEYCODE && callBack(),
 };
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-// eslint-disable-next-line no-unused-vars
-const checkStringLength = (string, length) => (string.length === length);
-
-export {getRandomInt, checkStringLength, keyboard};
+export {getRandomInt, keyboard};
